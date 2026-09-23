58b67ecb89568f6fa9a32e975276beb61b22b4a8
Kira <hocuz15@gmail.com>
2026-09-23 13:29:43 +0200

    updated style

--- script/script.js
+++ script/script.js
@@ -2,12 +2,12 @@ const links = document.querySelectorAll('.nav-links a');
 const sections = document.querySelectorAll('#main, #gallery, #contact');
 const aboutSection = document.querySelector('#about');
 
-// Скрываем все секции, кроме #main и #about
+
 sections.forEach(section => {
     section.style.display = 'none';
 });
 
-// Показываем главную и about по умолчанию
+
 document.querySelector('#main').style.display = 'flex';
 aboutSection.style.display = 'block';
 
@@ -17,18 +17,18 @@ links.forEach(link => {
 
         const targetID = link.getAttribute('href').replace('#', '');
 
-        // Скрываем всё
+      
         sections.forEach(section => {
             section.style.display = 'none';
         });
         aboutSection.style.display = 'none';
 
-        // Показываем нужное
+        
         const targetSection = document.getElementById(targetID);
 
         if (targetID === 'main') {
             targetSection.style.display = 'flex';
-            aboutSection.style.display = 'block'; // показываем about вместе с main
+            aboutSection.style.display = 'block'; 
         } else if (targetID === 'about') {
             targetSection.style.display = 'block';
         } else {
